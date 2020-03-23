0f4424e73f0b07ac2d0f53c0c5f9cb12da3f5885
Enmanuel Dario Ramirez Villamizar <e.ramirez.villamizar@accenture.com>
2020-03-23 12:54:42 -0300

    env commented in master

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+//require('dotenv').config();
 const Sequelize = require('sequelize');
 
 module.exports = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
